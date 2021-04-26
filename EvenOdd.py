cdb1fa9ba99e18acab5ce764347e167a532b925b
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2021-04-26 16:46:17 +0530

    Add files via upload

---
+++ EvenOdd.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 14:19:06 2020
+
+@author: LearnLeap
+"""
+
+num= int(input("Enter any Number"))
+
+num2= num % 2
+
+if num2==0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
